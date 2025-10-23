c6a2f0c91d6f38312aa8c36d48ffc6c03e170df2
joselo <jose@intuitivo.ai>
2025-10-22 21:26:26 -0700

    add test set to training of course

--- src/train.py
+++ src/train.py
@@ -1,38 +1,66 @@
 from model import ConvNet
-from datasets import load_dataset
-from tqdm import tqdm
-from utils import training_plots
 import numpy as np
-
-mnist = load_dataset("mnist")
+from tqdm import tqdm
+from utils import training_plots, download_data, preprocess_data
 
 model = ConvNet()
 
-num_samples = 100
-learning_rate = 0.001
-num_epochs = 10
+LEARNING_RATE = 0.001
+NUM_EPOCHS = 3
+TRAIN_SIZE = 600 # TODO
+TEST_SIZE = 100 # TODO
 
-total_loss_list = []
-accuracies = []
+train_loss_list = []
+train_accuracies = []
+val_loss_list = []
+val_accuracies = []
 
-for epoch in range(num_epochs):
-    total_loss = 0
-    num_correct = 0
-    for idx in tqdm(range(num_samples)):
-        x = mnist['train'][idx]['image']
-        x = np.array(x)
-        y = mnist['train'][idx]['label']
-        correct, loss = model.train_step(x, y, learning_rate)
-        total_loss += loss
-        if correct:
-            num_correct += 1
+if __name__ == '__main__':
+    train_images, train_labels, test_images, test_labels = download_data()
+    train_images, train_labels = preprocess_data(train_images, train_labels, TRAIN_SIZE)
+    test_images, test_labels = preprocess_data(test_images, test_labels, TEST_SIZE)
 
-    accuracy = num_correct / num_samples
-    avg_loss = total_loss / num_samples
-    print(f'Epoch {epoch}, Accuracy: {accuracy}, Loss: {avg_loss}')
+    # FASE DE ENTRENAMIENTO
+    for epoch in range(NUM_EPOCHS):
+        train_total_loss = 0
+        train_num_correct = 0
+        val_total_loss = 0
+        val_num_correct = 0
+        print('Training...')
+        for idx in tqdm(range(len(train_images))):
+            x = train_images[idx]
+            y = train_labels[idx]
+            correct, loss = model.train_step(x, y, LEARNING_RATE)
+            train_total_loss += loss
+            if correct:
+                train_num_correct += 1
 
-    total_loss_list.append(avg_loss)
-    accuracies.append(accuracy)
-    print(total_loss_list)
+        train_accuracy = train_num_correct / len(train_images)
+        train_avg_loss = train_total_loss / len(train_images)
+        
 
-training_plots(total_loss_list, accuracies, num_epochs)
+        train_loss_list.append(train_avg_loss)
+        train_accuracies.append(train_accuracy)
+        # FASE DE VALIDACIÓN
+        # Solo evaluamos forward para no modificar los pesos
+        print('Validation...')
+        for idx in tqdm(range(len(test_images))):
+            x = test_images[idx]
+            y = test_labels[idx]
+
+            y_pred = model.forward(x)
+            loss = model.loss(y_pred, y)
+            prediction = np.argmax(y_pred)
+            val_total_loss += loss
+            if prediction == y:
+                val_num_correct += 1
+        val_accuracy = val_num_correct / len(test_images)
+        val_avg_loss = val_total_loss / len(test_images)
+    
+        val_loss_list.append(val_avg_loss)
+        val_accuracies.append(val_accuracy)
+
+        print(f'Train Accuracy: {train_accuracy}, Train Loss: {train_avg_loss:.4f}')
+        print(f'Validation Accuracy: {val_accuracy}, Validation Loss: {val_avg_loss:.4f}')
+
+    training_plots(train_loss_list, train_accuracies, val_loss_list, val_accuracies, NUM_EPOCHS)
--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,49 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import os
+from datasets import load_dataset
 
-def training_plots(total_loss, accuracies, num_epochs):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+def training_plots(train_loss_list, train_accuracies, val_loss_list, val_accuracies, num_epochs):
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 10))
     
-    ax1.plot(np.arange(num_epochs), total_loss, 'g')
+    ax1.plot(np.arange(num_epochs), train_loss_list, 'g')
     ax1.set_xlabel('Epoch')
     ax1.set_ylabel('Loss')
     ax1.set_title('Loss over Epochs')
     
-    ax2.plot(np.arange(num_epochs), accuracies, 'b')
+    ax2.plot(np.arange(num_epochs), train_accuracies, 'b')
     ax2.set_xlabel('Epoch')
     ax2.set_ylabel('Accuracy')
     ax2.set_title('Accuracy over Epochs')
+
+    ax3.plot(np.arange(num_epochs), val_loss_list, 'r')
+    ax3.set_xlabel('Epoch')
+    ax3.set_ylabel('Loss')
+    ax3.set_title('Validation Loss over Epochs')
+
+    ax4.plot(np.arange(num_epochs), val_accuracies, 'y')
+    ax4.set_xlabel('Epoch')
+    ax4.set_ylabel('Accuracy')
+    ax4.set_title('Validation Accuracy over Epochs')
     
     plt.tight_layout()
     os.makedirs('results', exist_ok=True)
     plt.savefig('results/training_plots.png')
+
+def download_data():
+    print('Downloading data...')
+    mnist = load_dataset("mnist")
+    train_images = mnist['train']['image']
+    train_labels = mnist['train']['label']
+    test_images = mnist['test']['image']
+    test_labels = mnist['test']['label']
+    return train_images, train_labels, test_images, test_labels
+
+def preprocess_data(images, labels, size):
+    list_images = []
+    list_labels = []
+    for i in range(size):
+        converted_image = np.array(images[i])
+        list_images.append(converted_image)
+        list_labels.append(labels[i])
+    return list_images, list_labels
